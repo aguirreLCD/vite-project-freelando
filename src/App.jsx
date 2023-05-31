27f27af73543a9c0ffc859fb909ad874dce3b43f
Lilian Daminelli Aguirre <lilidaminelli@gmail.com>
2023-05-31 14:58:33 +0200

    new component created to unbind html style semantics: Typography

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Card } from "./components/Card/Card.jsx";
 import { GlobalStyles } from "./components/GlobalStyles/GlobalStyles.jsx";
 import { Header } from "./components/Header/Header.jsx";
 import { ThemeProviderContext } from "./components/ThemeProviderContext/ThemeProviderContext.jsx";
+import { Typography } from "./components/Typography/Typografy.jsx";
 
 function App() {
   return (
@@ -9,7 +10,15 @@ function App() {
       <GlobalStyles />
       <Header />
       <Card>
-        <h1>Freelando</h1>
+        <Typography variantToBindStyle="h1" htmlElement="h1">
+          Freelando
+        </Typography>
+
+        <Typography variantToBindStyle="body" htmlElement="body">
+          Create your profile for free to start working with the best
+          freelancers. Then you can give more details about your demands and
+          your way of working.
+        </Typography>
       </Card>
     </ThemeProviderContext>
   );
---
+++ src/components/Typography/Typografy.jsx
@@ -0,0 +1,69 @@
+/* eslint-disable react/prop-types */
+import styled from "@emotion/styled";
+
+// unbind tags and styles
+const htmlElements = {
+  h1: "h1",
+  h2: "h2",
+  h3: "h3",
+  body: "p",
+  bodyBold: "strong",
+  body2: "p",
+  body2Bold: "strong",
+  subtitle: "p",
+};
+
+// bind styles
+const htmlElementStyles = {
+  h1: `
+        font-weight: 600;
+        font-size: 40px;
+        line-height: 49px;
+    `,
+  h2: `
+        font-weight: 600;
+        font-size: 32px;
+        line-height: 39px;
+    `,
+  h3: `
+        font-weight: 500;
+        font-size: 24px;
+        line-height: 29px;
+    `,
+  body: `
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 24px;
+    `,
+  bodyBold: `
+        font-weight: 700;
+        font-size: 20px;
+        line-height: 24px;
+    `,
+  body2: `
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 20px;
+    `,
+  body2Bold: `
+        font-weight: 700;
+        font-size: 16px;
+        line-height: 20px;
+    `,
+  subtitle: `
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 17px;
+    `,
+};
+
+// Component to unbide html style semantics
+export const Typography = ({ variantToBindStyle, htmlElement, children }) => {
+  const htmlTag = htmlElements[htmlElement];
+
+  const CurrentElement = styled[htmlTag]`
+    ${htmlElementStyles[variantToBindStyle]}
+  `;
+
+  return <CurrentElement>{children} </CurrentElement>;
+};
