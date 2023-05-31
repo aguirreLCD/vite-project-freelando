ae175b17b0752ec167bce1891fa5495fedf70d78
Lilian Daminelli Aguirre <lilidaminelli@gmail.com>
2023-05-31 14:18:36 +0200

    created Header component - non-visual component, no behavior

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Card } from "./components/Card/Card.jsx";
 import { GlobalStyles } from "./components/GlobalStyles/GlobalStyles.jsx";
+import { Header } from "./components/Header/Header.jsx";
 import { ThemeProviderContext } from "./components/ThemeProviderContext/ThemeProviderContext.jsx";
 
 function App() {
   return (
     <ThemeProviderContext>
       <GlobalStyles />
+      <Header />
       <Card>
         <h1>Freelando</h1>
       </Card>
